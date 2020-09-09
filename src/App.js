6108e6c454a4b83720bc76dc9b08bdc5e2c0940f
aymenbraiek <aymen.braiek@esprit.tn>
2020-09-09 18:42:42 +0100

    Creating the Project+Understanding State Data Changes -closes #1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React ,{Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h4 className="bg-primary text-whit test-center p-2">
-       To Do List
-     </h4>
-    </div>
-  );
+ class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    userName: "Adam"
+    
+    }
+    }
+
+changeStateData = () => {
+this.setState({
+userName: this.state.userName === "Adam" ? "Bob" : "grdgffg",
+
+})
+}
+render() {
+return (
+<div>
+<h4 className="bg-primary text-white text-center p-2">
+{ this.state.userName }'s To Do List
+</h4>
+<button className="btn btn-primary m-2"
+onClick={ this.changeStateData }>
+Change
+</button>
+</div>
+)
+};
 }
 
 export default App;
